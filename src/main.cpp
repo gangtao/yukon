e83d1e10718c787098f5bc56d7a15767f7f66ae8
gangtao <gang.tao@outlook.com>
2021-09-01 15:10:42 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,104 +5,158 @@
 
 #include "rxcpp/rx.hpp"
 
-void sample()
+typedef void (*FnPtr)();
+
+void demo_create()
 {
-    std::string helloJim = generateHelloString("Yukon");
-    std::cout << helloJim << std::endl;
-
     // create a obseverable of Row
+    printf("//! [create flow]\n");
+    auto flow = makeDataTableFlow(10, 300);
+
+    flow.subscribe(
+        [](Row v)
+        { showRow(v); },
+        []()
+        { std::cout << "Create flow completed!" << std::endl; });
+    printf("//! [create flow]\n\n");
+}
+
+void demo_filter()
+{
+    printf("//! [filter flow : where usage > 80 ]\n");
+    auto flow = makeDataTableFlow(10, 300).filter([](Row v)
+                                                  { return std::get<int>(v["usage"]) > 80; });
+
+    flow.subscribe(
+        [](Row v)
+        { showRow(v); },
+        []()
+        { std::cout << "filter flow completed!" << std::endl; });
+    printf("//! [filter flow ]\n\n");
+}
+
+void demo_max()
+{
+    printf("//! [max usage: where state is required ]\n");
+    auto local_state_max = 0;
+    auto flow = makeDataTableFlow(10, 300).map([&local_state_max](Row v)
+                                               {
+                                                   auto usage = std::get<int>(v["usage"]);
+                                                   if (usage > local_state_max)
+                                                   {
+                                                       local_state_max = usage;
+                                                       return usage;
+                                                   }
+                                                   else
+                                                   {
+                                                       return local_state_max;
+                                                   }
+                                               });
+
+    flow.subscribe(
+        [](int v)
+        { std::cout << "max usage is: " << v << std::endl; },
+        []()
+        { std::cout << "filter flow completed!" << std::endl; });
+    printf("//! [filter flow ]\n\n");
+}
+
+void demo_groupby()
+{
+    printf("//! [count and group by tag ]\n");
+    auto flow = makeDataTableFlow(10, 300).group_by([](Row v)
+                                                    { return std::get<std::string>(v["tag"]); },
+                                                    [](Row v)
+                                                    { return v; });
+
+    flow.subscribe(
+        [](rxcpp::grouped_observable<std::string, Row> g)
+        {
+            auto key = g.get_key();
+            g.count().subscribe(
+                [key](int v)
+                {
+                    std::cout << key << " : " << v << std::endl;
+                },
+                [key]()
+                { std::cout << key << " -  group completed!" << std::endl; });
+        },
+        []()
+        { std::cout << "Group by completed!" << std::endl; });
+    printf("//! [group by ]\n\n");
+}
+
+void demo_concat()
+{
+    printf("//! [concat ]\n");
+    auto flow1 = makeDataTableFlow(10, 300);
+    auto flow2 = makeDataTableFlow(10, 300);
+
+    auto flow = flow1.concat(flow2);
+
+    flow.subscribe(
+        [](Row v)
+        { showRow(v); },
+        []()
+        { std::cout << "concat flow completed!" << std::endl; });
+    printf("//! [concat ]\n\n");
+}
+
+void demo_window_sliding()
+{
+    printf("//! [window with processing time]\n");
+    int counter = 0;
+    auto flow = makeDataTableFlow(10, 300).window_with_time(std::chrono::milliseconds(900), rxcpp::observe_on_new_thread());
+
+    flow.subscribe(
+        [&counter](rxcpp::observable<Row> w)
+        {
+            int id = counter++;
+            printf("[window %d] Create window\n", id);
+            w.subscribe(
+                [](Row v)
+                { showRow(v); },
+                [id]()
+                { std::cout << "window " << id << " flow completed!" << std::endl; });
+        },
+        []()
+        { std::cout << "window flow completed!" << std::endl; });
+    printf("//! [window with processing time ]\n\n");
+}
+
+void wait()
+{
+    do
     {
-        printf("//! [Create flow]\n");
-        auto flow = makeDataTableFlow(10, 300);
+        std::cout << '\n' << "Press a key to continue..." ;
+    } while (std::cin.get() != '\n');
+}
 
-        flow.subscribe(
-            [](Row v)
-            { showRow(v); },
-            []()
-            { std::cout << "Create flow completed!" << std::endl; });
-        printf("//! [Create sflow]\n");
-    }
-
-    // filter the flow
+void demo(const std::map<std::string, FnPtr> &fmap)
+{
+    for (auto const &[key, val] : fmap)
     {
-        printf("//! [Filter flow : where usage > 80 ]\n");
-        auto flow = makeDataTableFlow(10, 300).filter([](Row v)
-                                                      { return std::get<int>(v["usage"]) > 80; });
-
-        flow.subscribe(
-            [](Row v)
-            { showRow(v); },
-            []()
-            { std::cout << "Filter flow completed!" << std::endl; });
-        printf("//! [Filter flow ]\n");
-    }
-
-    // group by
-    {
-        printf("//! [Count and Group by tag ]\n");
-        auto flow = makeDataTableFlow(10, 300).group_by([](Row v)
-                                                        { return std::get<std::string>(v["tag"]); },
-                                                        [](Row v)
-                                                        { return v; });
-
-        flow.subscribe(
-            [](rxcpp::grouped_observable<std::string, Row> g)
-            {
-                auto key = g.get_key();
-                g.count().subscribe(
-                    [key](int v)
-                    {
-                        std::cout << key << " : " << v << std::endl;
-                    },
-                    [key]()
-                    { std::cout << key << " -  group completed!" << std::endl; });
-            },
-            []()
-            { std::cout << "Group by completed!" << std::endl; });
-        printf("//! [Group by ]\n");
-    }
-
-    // concat
-    {
-        printf("//! [concat ]\n");
-        auto flow1 = makeDataTableFlow(10, 300);
-        auto flow2 = makeDataTableFlow(10, 300);
-
-        auto flow = flow1.concat(flow2);
-
-        flow.subscribe(
-            [](Row v)
-            { showRow(v); },
-            []()
-            { std::cout << "concat flow completed!" << std::endl; });
-        printf("//! [concat ]\n");
-    }
-
-    // processing time sliding window
-    {
-        printf("//! [window ]\n");
-        int counter = 0;
-        auto flow = makeDataTableFlow(10, 300).window_with_time(std::chrono::milliseconds(900), rxcpp::observe_on_new_thread());
-
-        flow.subscribe(
-            [&counter](rxcpp::observable<Row> w)
-            {
-                int id = counter++;
-                printf("[window %d] Create window\n", id);
-                w.subscribe(
-                    [](Row v)
-                    { showRow(v); },
-                    []()
-                    { std::cout << "concat flow completed!" << std::endl; });
-            },
-            []()
-            { std::cout << "concat flow completed!" << std::endl; });
-        printf("//! [window ]\n");
+        std::cout << "demo to run : " << key << std::endl;
+        wait();
+        val();
     }
 }
 
 int main(int, char **)
 {
-    sample();
+    std::string helloJim = generateHelloString("Yukon");
+    std::cout << helloJim << std::endl;
+
+    std::map<std::string, FnPtr> functionMap;
+
+    functionMap["0_demo_create"] = demo_create;
+    functionMap["1_demo_filter"] = demo_filter;
+    functionMap["2_demo_groupby"] = demo_groupby;
+    functionMap["3_demo_max"] = demo_max;
+    functionMap["4_demo_concat"] = demo_concat;
+    functionMap["5_demo_window_sliding"] = demo_window_sliding;
+
+    demo(functionMap);
+
     return 0;
 }
